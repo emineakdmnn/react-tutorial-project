cdd112a91fbc7e08f608f135a62c8a6303d16462
Emine Akduman <eakdmn@gmail.com>
2024-01-31 17:12:54 +0300

    refactoring

--- src/pages/Trend/Movie/index.js
+++ src/pages/Trend/Movie/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import Service from "../../../services/Service";
-import styles from "../styles.module.scss";
+import Service from '../../../services/Service';
+import styles from '../styles.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import {Autoplay, EffectFade, Mousewheel, Navigation, Pagination} from "swiper/modules";
 
 
-const TrendMovie= () => {
+const TrendMovie = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
     const [loading, setLoading] = useState(true);
     const [errorResponse, setErrorResponse] = useState(null);
     const [trendMovies, setTrendMovies] = useState([]);
@@ -13,6 +17,7 @@ const TrendMovie= () => {
         setLoading(true);
         trendMovieService.fetchTrendMovies().then(
             (response) => {
+                console.log(response.results)
                 setTrendMovies(response.results);
                 setErrorResponse(null);
                 setLoading(false);
@@ -32,17 +37,38 @@ const TrendMovie= () => {
     return (
         <div>
             <h2 className={(styles['trend-movie-header'])}>Trend Filmler</h2>
-            <div className={(styles['trend-movie-container'])}>
-                {trendMovies.map((movie) => (
-                    <div key={movie.id} className={(styles['trend-movie-card'])}>
-                        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
-                        <h3>{movie.title}</h3>
-                        <p>{movie.release_date}</p>
-                    </div>
-                ))}
-            </div>
+            <Swiper
+                slidesPerView={'auto'}
+                modules={[EffectFade, Navigation, Pagination, Autoplay, Mousewheel]}
+                effect="cards"
+                pagination={{
+                    clickable: true,
+                }}
+                mousewheel={{forceToAxis: true}}
+                speed={1000}
+                navigation
+                onSlideChange={(slide) => {
+                    setActiveIndex(slide.realIndex >= 0 ? slide.realIndex : 0);
+                }}
+            >
+                {trendMovies.map((movie, index) => {
+                    return (
+                        <SwiperSlide key={index} style={{width: 'auto', margin: '0px 10px'}}>
+                            <div className={(styles['trend-movie-card'])}>
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <h3>{movie.title}</h3>
+                                <p>{movie.release_date}</p>
+                            </div>
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+
         </div>
     );
-}
+};
 
 export default TrendMovie;
