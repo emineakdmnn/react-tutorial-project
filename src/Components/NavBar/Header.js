89a38b04f581a10962bbc799a4d4774de8acaf95
Emine Akduman <eakdmn@gmail.com>
2024-02-23 21:57:10 +0300

    header design completed

--- src/Components/NavBar/Header.js
+++ src/Components/NavBar/Header.js
@@ -1,39 +1,37 @@
 import React from "react";
-import {HeaderItem} from "./Movie/HeaderItem";
+import { HeaderItem } from "./Movie/HeaderItem";
 import PropTypes from "prop-types";
-
+import logo from "../../images/movie.png"; // Update if necessary
+import styles from "./style.module.scss";
 
 function Header(props) {
     const menuItems = [
-        {
-            title: 'Movies',
-            path: '/movies',
-        },
-        {
-            title: 'Series',
-            path: '/series',
-        },
-        {
-            title: 'Popular People',
-            path: '/popular-person'
-        }
-    ]
+        { title: "Movies", path: "/movies" },
+        { title: "Series", path: "/series" },
+        { title: "Popular People", path: "/popular-person" },
+    ];
 
     return (
-        <header>
-            <h3>{props.headerTitle}</h3>
-            <nav>
-                {menuItems.map((menuItem, index) => (
-                    <HeaderItem key={menuItem.title} title={menuItem.title} path={menuItem.path} style={{ margin: '10px', color: 'red' }} />
-                ))}
-            </nav>
-        </header>
+        <div className={styles.header}>
+            <header className={styles.headerContent}>
+                <img src={logo} alt="Logo" className={styles.logo} />
+                <nav className={styles.nav}>
+                    {menuItems.map((menuItem, index) => (
+                        <HeaderItem
+                            key={menuItem.title}
+                            title={menuItem.title}
+                            path={menuItem.path}
+                            className="header-item"
+                        />
+                    ))}
+                </nav>
+            </header>
+        </div>
     );
-
 }
 
-Header.propTypes={
-    headerTitle: PropTypes.string.isRequired
-}
+Header.propTypes = {
+    headerTitle: PropTypes.string.isRequired,
+};
 
 export default Header;
