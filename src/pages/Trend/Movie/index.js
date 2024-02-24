ff57b9769f5aaa58417fcf8846652200129cc82c
Emine Akduman <eakdmn@gmail.com>
2024-02-24 16:35:52 +0300

    refactoring

--- src/pages/Trend/Movie/index.js
+++ src/pages/Trend/Movie/index.js
@@ -7,6 +7,7 @@ const TrendMovie= () => {
     const [loading, setLoading] = useState(true);
     const [errorResponse, setErrorResponse] = useState(null);
     const [trendMovies, setTrendMovies] = useState([]);
+    const [hoveredCard, setHoveredCard] = useState(null);
     const trendMovieService = new Service();
 
     const contentLoad = () => {
@@ -29,15 +30,28 @@ const TrendMovie= () => {
         contentLoad();
     }, []);
 
+    const handleMouseEnter = (movieId) => {
+        setHoveredCard(movieId);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredCard(null);
+    };
+
     return (
         <div>
-            <h2 className={(styles['trend-header'])}>Trend Filmler</h2>
-            <div className={(styles['trend-container'])}>
+            <h2 className={styles['trend-header']}>Trend Filmler</h2>
+            <div className={styles['trend-container']}>
                 {trendMovies.map((movie) => (
-                    <div key={movie.id} className={(styles['trend-card'])}>
-                        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}/>
-                        <h3>{movie.title}</h3>
-                        <p>{movie.release_date}</p>
+                    <div
+                        key={movie.id}
+                        className={`${styles['trend-card']} ${hoveredCard === movie.id ? styles['hovered-card'] : ''}`}
+                        onMouseEnter={() => handleMouseEnter(movie.id)}
+                        onMouseLeave={handleMouseLeave}>
+                        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
+                        <div className={styles['movie-info']}>
+                            <h3>{movie.title}</h3>
+                        </div>
                     </div>
                 ))}
             </div>
